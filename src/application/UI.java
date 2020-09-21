8659f961711611c8332a156695042042371dbe94
PC da Gubee <gabrielrodriguesdecastro@gmail.com>
2020-09-21 12:56:35 -0300

    Queen

--- src/application/UI.java
+++ src/application/UI.java
@@ -50,17 +50,6 @@ public class UI {
         }
     }
 
-    public static void printBoard(ChessPiece[][] pieces) {
-        for (int i = 0; i < pieces.length; i++) {
-            System.out.print((8 - i) + " ");
-            for (int j = 0; j < pieces.length; j++) {
-                printPiece(pieces[i][j], false);
-            }
-            System.out.println();
-        }
-        System.out.println("  a b c d e f g h");
-    }
-
     public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
         printBoard(chessMatch.getPieces());
         System.out.println();
@@ -77,15 +66,30 @@ public class UI {
         }
     }
 
+    public static void printBoard(ChessPiece[][] pieces) {
+        for (int i = 0; i < pieces.length; i++) {
+            System.out.print((8 - i) + " | ");
+            for (int j = 0; j < pieces.length; j++) {
+                printPiece(pieces[i][j], false);
+            }
+            System.out.print("|");
+            System.out.println();
+        }
+        System.out.println("   —————————————————");
+        System.out.println("    a b c d e f g h");
+    }
+
     public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
         for (int i = 0; i < pieces.length; i++) {
-            System.out.print((8 - i) + " ");
+            System.out.print((8 - i) + " | ");
             for (int j = 0; j < pieces.length; j++) {
                 printPiece(pieces[i][j], possibleMoves[i][j]);
             }
+            System.out.print("|");
             System.out.println();
         }
-        System.out.println("  a b c d e f g h");
+        System.out.println("   —————————————————");
+        System.out.println("    a b c d e f g h");
     }
 
     private static void printPiece(ChessPiece piece, boolean background) {
